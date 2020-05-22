254a0a5b11b794d75fe7a75570291b0231a62d97
Vatsal Shah <shahvats@amazon.com>
2020-05-22 08:28:57 +0000

    Simplified event extraction logic for s3 triggers

--- event_consumer_transcribe_function/lambda_function.py
+++ event_consumer_transcribe_function/lambda_function.py
@@ -10,36 +10,14 @@ logger = logging.getLogger()
 level = logging.getLevelName(os.getenv('LOG_LEVEL', 'DEBUG'))
 logger.setLevel(level)
 
-def parse_arn(arn):
-    # http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
-    elements = arn.split(':', 5)
-    result = {
-        'arn': elements[0],
-        'partition': elements[1],
-        'service': elements[2],
-        'region': elements[3],
-        'account': elements[4],
-        'resource': elements[5],
-        'resource_type': None
-    }
-    if '/' in result['resource']:
-        result['resource_type'], result['resource'] = result['resource'].split('/',1)
-    elif ':' in result['resource']:
-        result['resource_type'], result['resource'] = result['resource'].split(':',1)
-    return result
-
 def lambda_handler(event, context):
     logger.info("Starting transcribe job")
     logger.debug(event)
     client = boto3.client('transcribe')
-    for obj in event["detail"]["resources"]:
-        if obj["type"] == "AWS::S3::Object":
-            object_arn = obj["ARN"]
-    logger.info(object_arn)
-    parsed_arn = parse_arn(object_arn)
-    logger.debug(parsed_arn)
-    transcription_job_name = parsed_arn["resource_type"] + "_" + parsed_arn["resource"].split(".", 1)[0]
-    media_file_uri = "s3://{bucket_name}/{object_key}".format(bucket_name=parsed_arn["resource_type"], object_key=parsed_arn["resource"])
+    bucket_name = event["detail"]["requestParameters"]["bucketName"]
+    object_key = event["detail"]["requestParameters"]["key"]
+    transcription_job_name = bucket_name + "_" + object_key.split(".", 1)[0]
+    media_file_uri = "s3://{bucket_name}/{object_key}".format(bucket_name=bucket_name, object_key=object_key)
     logger.debug(media_file_uri)
     output_bucket_name = os.environ["OUTPUT_BUCKET_NAME"]
     vocabulary_name = os.environ["TRANSCRIBE_CUSTOM_VOCABULARY"]
